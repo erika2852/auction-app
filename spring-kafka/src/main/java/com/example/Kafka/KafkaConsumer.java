02c3f24a0157c075254fc3f2c555e5efe8e08d42
erika2852 <siro28528593@gmail.com>
2022-10-17 15:11:26 +0900

    first test

---
+++ spring-kafka/src/main/java/com/example/Kafka/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.example.Kafka;
+
+import java.io.IOException;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    @KafkaListener(topics = "exam-topic", groupId = "foo")
+    public void consume(String message) throws IOException {
+        log.info("Consumed message : {}", message);
+    }
+}
---
+++ spring-kafka/src/main/java/com/example/Kafka/KafkaController.java
@@ -0,0 +1,33 @@
+package com.example.Kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(value = "/kafka")
+@Slf4j
+public class KafkaController {
+
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer){
+        this.producer = producer;
+    }
+
+
+    @PostMapping
+    @ResponseBody
+    public String sendMessage(@RequestParam String message) {
+        log.info("message : {}", message);
+        this.producer.sendMessage(message);
+
+        return "success";
+    }
+}
